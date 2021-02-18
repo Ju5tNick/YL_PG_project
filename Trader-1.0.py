e7315300c151626b347cdddfbe42f4409c0c16f4
tiom4eg <62618821+tiom4eg@users.noreply.github.com>
2021-02-18 20:37:42 +0300

    Update Trader-1.0.py

--- Trader-1.0.py
+++ Trader-1.0.py
@@ -1,13 +1,17 @@
+import random
+
 class Trader:
-    """Trader logic:
-    - initializing trader with some deals, each deal has price and stock
-    - trader can ONLY sell you something, if you have enough money AND item.stock > 0"""
     def __init__(self, name):
         self.name = name
-        self.deals = {Item: [0, 0]}  # item: [price, size] (somehow generates randomly)
+        self.deals = {}
+        for i in range(15):
+            iname = ""
+            for i in range(10):
+                iname += str(random.randint(0, 9))
+                self.deals[iname] = Weapon(*args, **kwargs)
 
     def sell(self, item, hero):
-        if hero.get_balance() >= self.deals[item][0]:  # balance check
+        if hero.get_balance() >= self.deals[item][0]:
             if len(hero.get_inventory()) < 40:  # inventory check
                 # ok
                 hero.add_item(item)  # item goes in >>
